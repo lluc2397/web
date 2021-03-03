c613f1d927f7ac018dcbfbda39e681dbf7ee30b0
lucas <lluc23@hotmail.com>
2021-03-02 23:44:02 -0600

    Posible new supuser

--- usuario/models.py
+++ usuario/models.py
@@ -1,10 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django_countries.fields import CountryField
 from django.db.models import URLField
 from django.core.mail import send_mail
 from phonenumber_field.modelfields import PhoneNumberField
 
+class CustomAccountManager(UserManager):
+
+    def create_superuser(self, username, email, password, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(username, email, password, **other_fields)
 
 class MyUser(AbstractUser):
     first_name = models.CharField("Nombre", max_length=150, blank=True)
